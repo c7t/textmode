798ebf0e5a3af0440f694c73439efd0b7cf450ab
Chad Talbott <chad.talbott@gmail.com>
2016-09-12 23:38:42 -0700

    Exercise 4: Returning multiple values with pointers
    ...and noticing when a key is pressed.

--- textmode.c
+++ textmode.c
@@ -5,7 +5,6 @@
 #include <termios.h>
 #include <unistd.h>
 #include <string.h>
-/* Need select() call below */
 #include <sys/select.h>
 
 int term_set_raw(int fd, struct termios *term) {
@@ -29,6 +28,28 @@ int term_restore(int fd, struct termios *term) {
 }
 
 
+/* get_char() waits until timeout given by 'tv' for a key press.  If a
+   key is pressed in time, the key is returned in 'ch' and get_char()
+   returns true.  Otherwise get_char() returns false. */
+bool get_char(int *ch, struct timeval *tv) {
+    fd_set rfds;
+    int retval;
+
+    FD_ZERO(&rfds);
+    FD_SET(fileno(stdin), &rfds);
+
+    retval = select(1, &rfds, NULL, NULL, tv);
+
+    if (retval == -1)
+      perror("select()");
+    else if (retval) {
+      *ch = getchar();
+    }
+
+    return retval > 0;
+}
+
+
 int main(int argc, char **argv) {
   int i = 0;
 
@@ -39,44 +60,17 @@ int main(int argc, char **argv) {
   term_set_raw(fileno(stdin), &in_term);
   term_set_raw(fileno(stdout), &out_term);
 
-  /* Now we have to initialize ch to something, because we're going
-     to go ahead and print something, even if the user never pressed
-     a key. (Before we always blocked (waited forever) in getchar()
-     waiting for them to press something.) */
   int ch = 0;
 
   while (true) {
-    fd_set rfds;
     struct timeval tv;
-    int retval;
-
-    /* Watch stdin (fd 0) to see when it has input. */
-    FD_ZERO(&rfds);
-    FD_SET(fileno(stdin), &rfds);
-
-    /* 60 FPS is 1,000,000 microseconds (or one second) / 60, this
-       isn't quite correct, as we'll have used up some of our frame
-       time computing in this loop and writing output, but we can
-       correct that later.  Right now we just don't want to wait too
-       long for input to come in. */
     tv.tv_sec = 0;
-    tv.tv_usec = 1000000 / 60;
+    tv.tv_usec = 1000000 / 60; /* 60 FPS */
 
-    /* Select basically says: wait for a maximum timeout for some data
-       to become ready on the file descriptors that I specify.  Here
-       we use it only to look for input on stdin. */
-    retval = select(1, &rfds, NULL, NULL, &tv);
-    /* Don't rely on the value of tv now! */
+    bool key_pressed = get_char(&ch, &tv);
 
-    if (retval == -1)
-      perror("select()");
-    else if (retval) {
-      /* FD_ISSET(0, &rfds) will be true. This means we can "safely"
-	 call getchar() and it will return immediately with the
-	 character that was pressed, it won't block. */
-      ch = getchar();
-    } /* else no key was pressed, we continue to "render" (in this
-	 case just print the last character received again */
+    if (key_pressed)
+      printf("pressed!\r\n");
 
     printf("%d: %d %c\r\n", i, ch, ch);
     if (ch == 'q')
